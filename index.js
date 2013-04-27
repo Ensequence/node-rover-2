9c72060d6004e9d5166c45bb3097368f59e647ae
Steven White <steven.white@ensequence.com>
2013-04-27 18:46:42 -0400

    Moving primary library logic to lib/rover

--- index.js
+++ index.js
@@ -7,20 +7,7 @@
 
 // ### Dependencies
 var config = require('./lib/config'),
-    init = require('./lib/init');
+    rover = require('./lib/rover');
 
 // ### Exports
-module.exports = (function () {
-    // ### connect(cb)
-    // Establishes connection with rover 2.0
-    //
-    // * `cb`: function to deliver socket to upon successful connection
-    function connect (cb) {
-        init(cb);
-    }
-
-    // Expose methods
-    return {
-        connect: connect
-    };
-})();
+module.exports = rover;
